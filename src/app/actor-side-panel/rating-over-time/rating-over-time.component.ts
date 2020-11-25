8dbd09296eaee515c2c9cff7f38cb6e0a4f49b9e
Archer6621 <shaad1@live.nl>
2020-11-25 19:28:29 +0100

    Add area graphs, proper colors, legends and fix svg bounds

--- src/app/actor-side-panel/rating-over-time/rating-over-time.component.ts
+++ src/app/actor-side-panel/rating-over-time/rating-over-time.component.ts
@@ -13,7 +13,7 @@ import { ScaleLinear } from 'd3';
 export class RatingOverTimeComponent implements OnInit {
   private width = 800;
   private height = 400;
-  private margin = { top: 20, right: 40, bottom: 30, left: 80 };
+  private margin = { top: 20, right: 200, bottom: 30, left: 80 };
   private graph_width = this.width - this.margin.left - this.margin.right;
   private upper_graph_height = this.height / 2 - this.margin.top - this.margin.bottom;
   private bottom_graph_height = this.height / 2 - this.margin.top - this.margin.bottom;
@@ -21,13 +21,14 @@ export class RatingOverTimeComponent implements OnInit {
   private xScaleElement: d3.Selection<any, any, any, any>;
   private yScaleRating: ScaleLinear<number, number, never>;
   private yScaleRatingElement: d3.Selection<any, any, any, any>;
+  private legendElement: d3.Selection<any, any, any, any>;
   private yScaleRevenue: ScaleLinear<number, number, never>;
   private yScaleRevenueElement: d3.Selection<any, any, any, any>;
   private innerElement: d3.Selection<any, any, any, any>;
-  private colors: string[] = ['rgba(255,0,0,1)', 'rgba(0,0,255,1)']
+
   constructor(private _actorRepository: ActorRepository) { }
 
-  appendActorGraph(actor: Actor, actorMovies: Movie[]): void {
+  appendActorGraph(actor: Actor, actorMovies: Movie[], index: number): void {
     let birth = new Date(actor.birth, 0, 0);
     let age_div = +new Date(actor.birth + 100, 0, 0) - +new Date(actor.birth, 0, 0);
     const data = this.innerElement.append('g').selectAll('g')
@@ -35,43 +36,99 @@ export class RatingOverTimeComponent implements OnInit {
       .join('g')
       .attr('transform', m => `translate(${this.xScale(100 * (+new Date(m.year, m.month, m.day) - +birth) / age_div)}, ${this.upper_graph_height})`);
 
-    let c = this.colors.pop();
+    let c = d3.color(d3.schemeSet2[index]);
+    let mainColor = c.copy();
+    let areaColor = c.copy();
+    areaColor.opacity = 0.1;
+    let stemColor = c.copy();
+    stemColor.opacity = 0.75;
+    let stemTopColor = c.copy().darker();
+    
+    
     data.append('line')
       .transition()
       .attr('y1', m => this.yScaleRating(m.vote_average / 2) - this.upper_graph_height)
       .duration(1000)
       .attr('y2', this.yScaleRating(0) - this.upper_graph_height)
-      .attr('stroke', c)
+      .attr('stroke', stemColor.formatRgb())
+      .attr('stroke-dasharray', 1)
 
     data.append('circle')
       .transition()
       .attr('cy', m => this.yScaleRating(m.vote_average / 2) - this.upper_graph_height)
       .duration(1000)
-      .attr('r', '2')
-      .attr('fill', c)
-      .attr('stroke', c)
+      .attr('r', '3')
+      .attr('fill', areaColor.formatRgb())
+      .attr('stroke', stemTopColor.formatRgb())
 
     data.append('line')
       .transition()
       .attr('y2', m => this.yScaleRevenue(m.revenue))
       .duration(1000)
       .attr('y1', this.yScaleRevenue(0))
-      .attr('stroke', c)
+      .attr('stroke', stemColor.formatRgb())
+      .attr('stroke-dasharray', 1)
 
     data.append('circle')
       .transition()
       .attr('cy', m => this.yScaleRevenue(m.revenue))
       .duration(1000)
-      .attr('r', '2')
-      .attr('fill', c)
-      .attr('stroke', c)
+      .attr('r', '3')
+      .attr('fill', areaColor.formatRgb())
+      .attr('stroke', stemTopColor.formatRgb());
+
+    const moviesSorted = actorMovies.sort((a, b) => +new Date(a.year, a.month, a.day) - +new Date(b.year, b.month, b.day))
+        
+    // Upper area
+    this.innerElement.append("path")
+      .datum(moviesSorted)
+      .attr("fill", areaColor.formatRgb())
+      .attr("stroke", mainColor.formatRgb())
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.area<Movie>()
+        .x(m => this.xScale(100 * (+new Date(m.year, m.month, m.day) - +birth) / age_div))
+        .y1(m => this.yScaleRating(m.vote_average / 2))
+        .y0(this.upper_graph_height)
+        .curve(d3.curveMonotoneX)
+      );
+
+    // Bottom area
+    this.innerElement.append("path")
+      .datum(moviesSorted)
+      .attr("fill", areaColor.formatRgb())
+      .attr("stroke", mainColor.formatRgb())
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.area<Movie>()
+        .x(m => this.xScale(100 * (+new Date(m.year, m.month, m.day) - +birth) / age_div))
+        .y1(m => this.yScaleRevenue(m.revenue) + this.upper_graph_height )
+        .y0(this.upper_graph_height)
+        .curve(d3.curveMonotoneX)
+      );
+
+      //  Update legend
+    this.legendElement.append('rect')
+      .attr('x', 20 + this.graph_width  )
+    .attr('y',  30 * index)
+    .attr('width', 20)
+    .attr('height', 20)
+    .attr('fill', mainColor.formatRgb())
+    .attr('stroke', stemTopColor.formatRgb())
+    this.legendElement.append('text')
+      .attr('x', 20 + this.graph_width + 30)
+      .attr('y',  30 * index + 10)
+      .attr('width', 100)
+      .attr('height', 20)
+      .attr('text-anchor', 'start')
+      .attr('fill', mainColor.formatRgb())
+      .attr('align', 'center')
+      .text(actor.name)
   }
 
-  lookupAndAddActor(name: string): void {
+  lookupAndAddActor(name: string, index: number): void {
     this._actorRepository.getActorByName(name).subscribe(actor => {
       forkJoin(this._actorRepository.getMovies(actor.movies)).subscribe(movies => {
         this.updateScales(actor, movies);
-        this.appendActorGraph(actor, movies);
+        this.appendActorGraph(actor, movies, index);
       })
     }, (err) => {
       console.error(err);
@@ -125,7 +182,7 @@ export class RatingOverTimeComponent implements OnInit {
       .range([0, this.bottom_graph_height]);
 
     const svg = d3.select('p#test').append('svg')
-      .attr('width', this.xScale.range()[1])
+      .attr('width', this.width)
       .attr('height', this.height)
       .attr('font-family', 'sans-serif')
       .attr('font-size', '10')
@@ -134,24 +191,32 @@ export class RatingOverTimeComponent implements OnInit {
     this.innerElement = svg.append('g')
       .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`)
 
-    this.xScaleElement = this.innerElement.append('g')
+    const axes = svg.append('g')
+      .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`)
+
+    this.legendElement = svg.append('g')
+      .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`)
+
+    this.xScaleElement = axes.append('g')
       .attr('transform', `translate(0,${this.upper_graph_height})`)
       .attr('shape-rendering', 'geometricPrecision ')
       .call(d3.axisBottom(this.xScale));
 
-    this.yScaleRatingElement = this.innerElement.append('g')
+    this.yScaleRatingElement = axes.append('g')
       .attr('transform', `translate(0,0)`)
       .attr('shape-rendering', 'geometricPrecision ')
       .call(d3.axisLeft(this.yScaleRating).ticks(5));
 
-    this.yScaleRevenueElement = this.innerElement.append('g')
+    this.yScaleRevenueElement = axes.append('g')
       .attr('transform', `translate(0,${this.upper_graph_height})`)
       .attr('shape-rendering', 'geometricPrecision ')
       .call(d3.axisLeft(this.yScaleRevenue).ticks(5, '$,.0s'));
 
-    const actors: string[] = ['Arnold Schwarzenegger', 'Will Smith'];
-    actors.forEach(a => {
-      this.lookupAndAddActor(a);
+
+
+    const actors: string[] = ['Bruce Willis', 'Will Smith'];
+    actors.forEach((a, i) => {
+      this.lookupAndAddActor(a, i);
     });
 
     // this._actorRepository.getActorByName(a1).subscribe(actor => {
